8e725977fb4d48131e6735c2bfd18318a251c091
Reinid <reinidval@gmail.com>
2025-09-27 14:34:03 -0300

    feature: adición de configuración para pruebas de API y creación del código para la primera prueba de API testing - creación de un issue

---
+++ tests/APITests/APITests.spec.ts
@@ -0,0 +1,23 @@
+ import { test, expect} from '@playwright/test';
+
+ const REPO = 'playwright-frt-curso';
+ const USER = 'ProgrammerDwarf';
+
+ test('Se puede crear un Issue en el repositorio de Github', async ({ request }) => {
+    const newIssue = await request.post(`/repos/${USER}/${REPO}/issues`, {
+        data: {
+            title: '[Bug] reporte algo nuevo',
+            body: 'Descripcion del bug',
+        }
+    });
+
+    expect(newIssue.status()).toBe(201);
+
+     const issues = await request.get(`/repos/${USER}/${REPO}/issues`);
+     expect(issues.ok()).toBeTruthy();
+     expect(await issues.json()).toContainEqual(expect.objectContaining({
+        title: '[Bug] reporte algo nuevo',
+        body: 'Descripcion del bug',
+     }));
+ });
+ 

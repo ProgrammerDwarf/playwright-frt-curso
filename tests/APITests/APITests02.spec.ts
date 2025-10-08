2b73a679898ad06bb85abe42272918871f71d522
Reinid <reinidval@gmail.com>
2025-10-07 21:59:42 -0300

    feat: agregar APItest03 como la clase siguiente, en la que creamos un repo, insertamos un nuevo issue y luego borramos el repo

---
+++ tests/APITests/APITests02.spec.ts
@@ -0,0 +1,42 @@
+ import { test, expect, } from '@playwright/test';
+
+ const REPO = 'REPO-LOCO-NUEVO';
+ const USER = 'ProgrammerDwarf';
+
+ const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+ test.beforeAll(async ({ request }) => {
+    const response = await request.post('user/repos', {
+        data: {
+            name: REPO
+        }
+    });
+    expect(response.ok()).toBeTruthy();
+ })
+ 
+ test('Se puede crear un Issue en el repositorio de Github', async ({ request }) => {
+    const newIssue = await request.post(`/repos/${USER}/${REPO}/issues`, {
+        data: {
+            title: '[Bug] reporte algo nuevo',
+            body: 'Descripcion del bug',
+        }
+    });
+
+    expect(newIssue.status()).toBe(201);
+
+    await sleep(2000);
+
+     const issues = await request.get(`/repos/${USER}/${REPO}/issues`);
+     expect(issues.ok()).toBeTruthy();
+     expect(await issues.json()).toContainEqual(expect.objectContaining({
+        title: '[Bug] reporte algo nuevo',
+        body: 'Descripcion del bug',
+     }));
+ });
+
+ test.afterAll(async ({ request }) => {
+    const response = await request.delete(`/repos/${USER}/${REPO}`);
+    expect(response.ok()).toBeTruthy();
+ });
+ 
+ 
